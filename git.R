a00825e2af6d14a5cef98ad7b87f12b7dda80e1a
Mateussouzah <mateussouzah@gmail.com>
2022-09-09 14:25:25 -0300

    atualização

---
+++ git.R
@@ -0,0 +1,11 @@
+usethis::use_git_config(user.name = "Mateussouzah",
+                        user.mail = "mateussouzah@gmail.com")
+usethis::use_git()
+
+
+usethis::create_github_token()
+
+
+gitcreds::gitcreds_set()
+
+usethis::use_github()
